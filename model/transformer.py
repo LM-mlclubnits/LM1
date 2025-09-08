39206ff1447a2c1156fc1dab54a4634648792c91
Bhumisti2006 <bhumisti_ug_24@cse.nits.ac.in>
2025-09-08 21:59:29 +0530

    optimised

--- model/transformer.py
+++ model/transformer.py
@@ -1,18 +1,24 @@
 import torch.nn as nn
 
-
 class TransformerBlock(nn.Module):
-    def __init__(self, d_model, n_heads, d_ff):
+    def __init__(self, d_model, n_heads, d_ff, dropout=0.1):
         super().__init__()
-        self.attn = nn.MultiheadAttention(d_model, n_heads, batch_first = True)
         self.ln1 = nn.LayerNorm(d_model)
-        self.ff = nn.Sequential(
-            nn.Linear(d_model, d_ff), nn.ReLU(), nn.Linear(d_ff, d_model)
-        )
+        self.attn = nn.MultiheadAttention(d_model, n_heads, batch_first=True, dropout=dropout)
         self.ln2 = nn.LayerNorm(d_model)
+        self.ff = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.GELU(),                        
+            nn.Dropout(dropout),
+            nn.Linear(d_ff, d_model),
+            nn.Dropout(dropout),
+        )
 
     def forward(self, x, mask=None):
-        attn_out, _ = self.attn(x,x,x, attn_mask=mask)
-        x = self.ln1(x + attn_out)
-        ff_out = self.ff(x)
-        return self.ln2(x + ff_out)
+        x_norm = self.ln1(x)
+        attn_out, _ = self.attn(x_norm, x_norm, x_norm, attn_mask=mask)
+        x = x + attn_out                      
+
+        x_norm = self.ln2(x)
+        ff_out = self.ff(x_norm)
+        return x + ff_out                    
